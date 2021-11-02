85bf3ff5a53a1bb64849f9c23e8e7711dcdc9bf9
talnor <tanja.normark@scilifelab.se>
2021-11-02 14:08:06 +0100

    Comment out use of master for submodules

--- mutant/standalone/deploy_hasta_update.sh
+++ mutant/standalone/deploy_hasta_update.sh
@@ -28,8 +28,8 @@ git pull origin master
 cd ${OLDDIR}
 git submodule init
 git submodule update
-git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
+#git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
 
 # Pull latest image
-singularity pull --force /home/proj/${INSTANCE}/mutant/MUTANT/mutant/externals/gms-artic/artic-ncov2019-illumina.sif docker://clinicalgenomics/artic-ncov2019-illumina:latest
+singularity pull --force mutant/externals/gms-artic/artic-ncov2019-illumina.sif docker://clinicalgenomics/artic-ncov2019-illumina:latest
 chmod u=rwx,g=rx,o=rx mutant/externals/gms-artic/artic-ncov2019-illumina.sif
