dfb3bbbeaca2efac1b041d19530e9fb8069f8e1d
henningonsbring <henning.onsbring@gmail.com>
2021-12-15 18:58:07 +0100

    parsing or result

--- mutant/modules/artic_nanopore/parser.py
+++ mutant/modules/artic_nanopore/parser.py
@@ -1,38 +1,80 @@
 """Parse SARS-CoV-2 nanopore results"""
-from mutant.modules.generic_parser import (
-    get_sarscov2_config,
-)
+import os
+
+from pathlib import Path
+from mutant.modules.generic_parser import get_sarscov2_config
 
 
 class ParserNanopore:
-    def __init__(self, caseinfo, indir):
-        caseinfo = get_sarscov2_config(caseinfo)
+    def __init__(self, caseinfo: str, indir: Path):
         self.caseinfo = caseinfo
         self.indir = indir
 
-    def collect_results(self) -> dict:
-        results: dict = self.parse_config()
-        results: dict = self.parse_assembly(results=results)
-        results: dict = self.calculate_coverage(results=results)
-        results: dict = self.parse_pangolin(results=results)
-        results: dict = self.parse_classifications(results=results)
-        results: dict = self.parse_mutations(results=results)
+    def translate_barcodes(self, parsed_config: dict) -> dict:
+        """Builds a dict where barcodes point at customer sample id"""
+        barcode_to_sample = {}
+        for sample in parsed_config:
+            barcode_to_sample[sample["barcode"]] = sample["Customer_ID_sample"]
+        return barcode_to_sample
+
+    def get_data_from_config(self, parsed_config: dict) -> dict:
+        """Collect data for selection criteria and region"""
+        data_to_report = {}
+        for sample in parsed_config:
+            cust_sample_id = sample["Customer_ID_sample"]
+            data_to_report[cust_sample_id]["selection_criteria"] = sample["selection_criteria"]
+            data_to_report[cust_sample_id]["region_code"] = sample["region_code"]
+        return data_to_report
+
+#    def parse_assembly(self, results: dict) -> dict:
+#        pass
+
+#    def calculate_coverage(self, results: dict) -> dict:
+#        pass
+
+    def get_second_line(self, filename: str) -> str:
+        """Return the second line of the file"""
+        opened_file = open(filename)
+        second_line = ""
+        for i, line in enumerate(opened_file):
+            if i == 1:
+                second_line = line
+        return second_line
+
+    def get_cust_sample_id(self, raw_pangolin_result: str) -> str:
+        split_on_slash = raw_pangolin_result.split("/")
+        sample_folder = split_on_slash[0]
+        split_on_underscore = sample_folder.split("_")
+        cust_sample_id = split_on_underscore[-1]
+        return cust_sample_id
+
+    def get_pangolin_type(self, raw_pangolin_result: str) -> str:
+        split_on_comma = raw_pangolin_result.split(",")
+        lineage = split_on_comma[1]
+        return lineage
+
+    def parse_pangolin(self, results: dict, barcode_to_sample: dict) -> dict:
+        """Collect data for pangolin types"""
+        for filename in os.listdir("/home/hiseq.clinical/HO_data_processing/projects/nanopore/outputs/output_14/articNcovNanopore_sequenceAnalysisMedaka_articMinIONMedaka/"):
+            second_line: str = self.get_second_line(filename=filename)
+            cust_sample_id: str = self.get_cust_sample_id(raw_pangolin_result=second_line)
+            pangolin_type: str = self.get_pangolin_type(raw_pangolin_result=second_line)
+            results[cust_sample_id]["pangolin_type"] = pangolin_type
         return results
 
-    def parse_config(self) -> dict:
-        pass
+#    def parse_classifications(self, results: dict) -> dict:
+#        pass
 
-    def parse_assembly(self, results: dict) -> dict:
-        pass
+#    def parse_mutations(self, results: dict) -> dict:
+#        pass
 
-    def calculate_coverage(self, results: dict) -> dict:
-        pass
-
-    def parse_pangolin(self, results: dict) -> dict:
-        pass
-
-    def parse_classifications(self, results: dict) -> dict:
-        pass
-
-    def parse_mutations(self, results: dict) -> dict:
-        pass
+    def collect_results(self) -> dict:
+        parsed_config: dict = get_sarscov2_config(config=self.caseinfo)
+        barcode_to_sample: dict = self.translate_barcodes(parsed_config=parsed_config)
+        results: dict = self.get_data_from_config(parsed_config=parsed_config)
+        #results: dict = self.parse_assembly(results=results)
+        #results: dict = self.calculate_coverage(results=results)
+        results: dict = self.parse_pangolin(results=results, barcode_to_sample=barcode_to_sample)
+        #results: dict = self.parse_classifications(results=results)
+        #results: dict = self.parse_mutations(results=results)
+        return results
--- mutant/modules/artic_nanopore/report.py
+++ mutant/modules/artic_nanopore/report.py
@@ -1,13 +1,18 @@
 """ Using a dict as input, this class will print a report covering the
     information requested by the sarscov2-customers at Clinical Genomics
 """
-
+from mutant.modules.generic_parser import (
+    get_sarscov2_config,
+)
 
 class ReportPrinterNanopore:
-    def __init__(self, indir):
+    def __init__(self, indir, caseinfo):
+        self.casefile = caseinfo
+        caseinfo = get_sarscov2_config(caseinfo)
+        self.case = caseinfo[0]["case_ID"]
+        self.ticket = caseinfo[0]["Customer_ID_project"]
+        self.project = caseinfo[0]["Customer_ID_project"]
         self.indir = indir
 
     def print_report(self, result: dict) -> None:
         pass
-
-
