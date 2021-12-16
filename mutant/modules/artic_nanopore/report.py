97f36662c5d166e1b9fd6d1240a10d245150cc68
henningonsbring <henning.onsbring@gmail.com>
2021-12-16 13:50:25 +0100

    getting keys in lists

--- mutant/modules/artic_nanopore/report.py
+++ mutant/modules/artic_nanopore/report.py
@@ -1,6 +1,7 @@
 """ Using a dict as input, this class will print a report covering the
     information requested by the sarscov2-customers at Clinical Genomics
 """
+from collections import abc
 from pathlib import Path
 
 from mutant.modules.generic_parser import get_sarscov2_config
@@ -14,6 +15,9 @@ class ReportPrinterNanopore:
         self.indir = indir
 
     def print_report(self, result: dict) -> None:
-        for sample, key in result.items():
-            message = "Sample {0} with key {1} has value {2}".format(sample, key, result[sample][key])
-            print(message)
+        samples = result.keys()
+        for sample in samples:
+            analysis_data = result[sample].keys()
+            for data_point in analysis_data:
+                message = "{0} for sample {1} has value {2}".format(data_point, sample, result[sample][data_point])
+                print(message)
