6233c3c557ada3cadc27bc86f48e55f9b32a2aff
henningonsbring <henning.onsbring@scilifelab.se>
2022-04-10 14:45:02 +0200

    changed columns that information is collected from

--- mutant/modules/artic_illumina/parser.py
+++ mutant/modules/artic_illumina/parser.py
@@ -200,8 +200,8 @@ def get_artic_results(indir) -> dict:
                     {
                         "lineage": line[1],
                         "pangolin_probability": line[3],
-                        "pangoLEARN_version": line[-4],
-                        "pangolin_qc": line[-2],
+                        "pangoLEARN_version": line[-7],
+                        "pangolin_qc": line[-3],
                     }
                 )
 
