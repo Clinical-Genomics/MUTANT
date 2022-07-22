62dfeef41423d7a656b1aa8abc2bbfdfbb4ee9b0
henningonsbring <henning.onsbring@scilifelab.se>
2022-07-22 15:27:54 +0200

    fixed .fasta .fa confusion

--- mutant/modules/generic_reporter.py
+++ mutant/modules/generic_reporter.py
@@ -37,8 +37,11 @@ class GenericReporter:
 
     def create_concat_consensus(self, target_files: str):
         """Concatenate consensus files"""
-
-        concat = open("{0}/{1}.consensus.fa".format(self.indir, self.ticket), "w+")
+        if self.nanopore:
+            concat_consensus = "{0}/{1}.consensus.fasta".format(self.indir, self.ticket)
+        else:
+            concat_consensus = "{0}/{1}.consensus.fa".format(self.indir, self.ticket)
+        concat = open(concat_consensus, "w+")
         for item in glob.glob(target_files):
             single = open(item, "r")
             concat.write(single.read())
